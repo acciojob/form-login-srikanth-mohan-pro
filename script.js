e9760687b5e1bde3fdfc77a42de5cf348fabea02
Srikanth Mohan <srikanth.mohan.98@gmail.com>
2026-02-26 11:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(event) {
+	event.preventDefault();
+    let firstname=document.getElementsbyName("fname")[0].value.trim();
+	let lastname=document.getElementsbyName("lname")[0].value.trim();
+	alert(firstname+" "+lastname);
 }
